16ca694a881e6e317e513c53dc0be321439337c8
Anand <xlr8blaze@gmail.com>
2022-09-05 17:13:38 +0530

    Flask app created

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pickle
-from django.shortcuts import render
+
 from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
@@ -7,16 +7,22 @@ import pandas as pd
 app = Flask(__name__)
 
 # Load the model
-model = pickle.load(open('regmodel.pkl', 'rb'))
+regmodel = pickle.load(open('regmodel.pkl', 'rb'))
+scaler = pickle.load(open('scaling.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html') # will redirect ro home page by looking into the templates folder
 
-@app.route('/predict_api', methods=['POST'])
+@app.route('/predict_api', methods=['POST']) # using this as an api itself, will be using with postman
 def predict_api():
     data = request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1, -1))
-    new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
-    
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
+    output = regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__ == "__main__":
+    app.run(debug=True)
